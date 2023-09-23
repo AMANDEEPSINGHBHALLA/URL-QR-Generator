a9c3937022d371273aeacb390379d35b282f76a3
AMANDEEP SINGH BHALLA <amandeepsinghbhalla.ab@outlook.com>
2023-09-23 10:16:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,72 @@ import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 2000;
+const port = 3000;
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-app.post("/url", (req, res) => {
-  const URL = req.body.urlinput;
-  console.log(URL);
-  var qr_svg = qr.image(URL);
-  qr_svg.pipe(fs.createWriteStream("public/img/qr_img.png"));
-
-  res.sendFile(__dirname + "/public/qr.html");
+//connecting database
+mongoose.connect("mongodb://127.0.0.1:27017/urlqrDB")
+.then(()=> {
+    console.log("successfully connected");
+})
+.catch((err)=> {
+    console.log("Error", err);
 });
 
-
-  app.listen(port, () => {
-    console.log(`running on port ${port}`);
+//schema
+const urlSchema = new mongoose.Schema({
+    link: {
+      type: String,
+      required: true,
+    }
   });
+
+//model
+const uLink = mongoose.model("uLink", urlSchema);
+
+app.get("/", (req, res)=> {
+    uLink.find()
+    .then((sData)=> {
+        if (sData.length == 0) {
+            res.render("index.ejs");
+        }
+        else if (sData.length > 5) {
+            uLink.deleteOne({__v: 0}).then(()=> {
+                res.redirect("/");
+            }).catch((err)=> {
+                console.log(err);
+            });
+        }
+        else {
+            res.render("index.ejs", {
+                findP: sData
+            });
+        }
+    }).catch((err)=> {
+        console.log(err);
+    });
+});
+
+app.post("/url", (req, res)=> {
+    const URL = req.body.urlinput;
+
+    const test = new uLink({
+        link: URL
+      });
+      test.save();
+
+    console.log(URL);
+    var qr_svg = qr.image(URL);
+    qr_svg.pipe(fs.createWriteStream("public/img/qr_img.png"));
+
+    res.render("qr.ejs");
+});
+
+app.listen(port, ()=> {
+    console.log(`running on port ${port}`);
+});
